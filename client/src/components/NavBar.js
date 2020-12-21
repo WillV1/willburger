b3f8680543eba564cc1f2e78679f802be937da05
WillV1 <wmvanhook@gmail.com>
2020-12-21 18:33:02 -0500

    Add search component

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React, { useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { logout } from '../actions/auth';
@@ -12,29 +13,49 @@ const NavBar = ({auth: {isAuthenticated, user, loading}, logout}) => {
 
   const authLinks = (
     <Fragment>
-    {/* Dropdown Structure */}
-    <ul id="dropdown1" class="dropdown-content">
-    <li><a href="#!">Place Order</a></li>
-    <li><a href="#!">Current Order</a></li>
-    <li><a href="#!">Account</a></li>
-    </ul>
 
-    <ul class="right hide-on-med-and-down">
-          <li><a href="sass.html">Place Order</a></li>
-          <li><a href="badges.html">Locations</a></li>
-          {/*} Dropdown Trigger */}
-          <li><a className="dropdown-trigger" href="#!" data-target="dropdown1"><span>{user ? `Hi ${user.username} !`: ''}</span><i class="material-icons right">arrow_drop_down</i></a></li>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <span>{user ? `Hi ${user.username} !`: ''}</span>
+        <li><Link to="/search">Search New Recipes</Link></li>
+        <li><a href="badges.html">Add Recipe</a></li>
+        <li><a href="badges.html">Edit Recipe</a></li>
+        <li><Link onClick={logout} to="/">Log Out</Link></li>       
+      </ul>
+
+      <ul id="slide-out" className="sidenav">
+        <li><Link to="/search">Search New Recipes</Link></li>
+        <li><a href="badges.html">Add Recipe</a></li>
+        <li><a href="badges.html">Edit Recipe</a></li>
+        <li><Link onClick={logout} to="/">Log Out</Link></li>  
+      </ul>
+      <a href="/" data-target="slide-out" className="sidenav-trigger">
+      <i className="material-icons">menu</i></a>
+    </Fragment>
+  )
+
+  const guestLinks = (
+    <Fragment>
+
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><Link to="/register">Register</Link></li>
+          <li><Link to="/login">Login</Link></li>
         </ul>
+
+        <ul id="slide-out" className="sidenav">
+          <li><Link to="/register">Register</Link></li>
+          <li><Link to="/login">Login</Link></li>
+      </ul>
+      <a href="/" data-target="slide-out" className="sidenav-trigger">
+      <i className="material-icons">menu</i></a>
     </Fragment>
   )
 
   return (
     <div>
-    
       <nav>
       <div className="nav-wrapper">
-        <a href="#!" className="brand-logo">WillBurger</a>
-        {!loading && <Fragment>{isAuthenticated ? authLinks : ''}</Fragment>}
+        <Link to="/" className="brand-logo">Recipe Tracker</Link>
+        {!loading && <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>}
       </div>
       </nav>
     </div>
--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,14 +1,77 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getRecipes } from '../actions/recipe';
+import RecipeItem from '../components/RecipeItem';
 import Spinner from '../layout/Spinner';
 
 const SearchPage = ({getRecipes, recipe:{recipes, loading}}) => {
+
+  const [state, setState] = useState({
+    ingredient: '',
+    query: '',
+    page: ''
+  });
+
+  const {  ingredient, query, page } = state;
+
+  useEffect(() => {
+    getRecipes();
+  }, [getRecipes]);
+
+  const onChange = e => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+
+
+
+    setState({
+      ingredient: '',
+      query: '',
+      page: ''
+    })
+  };
+
+
+
   return (
     <div>
-    
-    </div>
+    <div class="row">
+    <form class="col s12" onSubmit={e => onSubmit(e)}>
+      <div class="row">
+        <div class="input-field col s6">
+          <input placeholder="Placeholder" id="first_name" type="text" class="validate" />
+          <label for="first_name">First Name</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="last_name" type="text" class="validate" />
+          <label for="last_name">Last Name</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate" />
+          <label for="password">Password</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate" />
+          <label for="email">Email</label>
+        </div>
+      </div>
+    </form>
+  </div>
+  {recipes.map(recipe => {
+    <RecipeItem />
+  })}
+  </div>
   )
 }
 
